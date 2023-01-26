a6ee9aabfbc8d91e8ecd1e277233e16079b18636
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-01-26 14:34:55 +0900

    [Silver IV] Title: 제로, Time: 3892 ms, Memory: 31248 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,14 +1,9 @@
-T = int(input())
-money_list = []
-for _ in range(T):
-    N = int(input())
-    money_list.append(N)
-
-new_money_list = []
-
-for money in money_list:
-    if money == 0:
-        new_money_list.pop()
+N = int(input())
+number_list = []
+for _ in range(N):
+    number = int(input())
+    if number == 0:
+        number_list.pop()
     else:
-        new_money_list.append(money)
-print(sum(new_money_list))
+        number_list.append(number)
+print(sum(number_list))
