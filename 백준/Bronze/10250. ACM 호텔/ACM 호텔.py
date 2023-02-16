152427a729f079c8f098d8b306290eaead5692f2
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-02-17 01:38:24 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -5,8 +5,4 @@ for _ in range(int(input())):
     if front_num == 0:
         front_num = h
         back_num -= 1
-    front_num = str(front_num)
-    back_num = str(back_num)
-    if len(back_num) == 1:
-        back_num = "0" + back_num
-    print(front_num + back_num)
+    print((front_num * 100) + back_num)
