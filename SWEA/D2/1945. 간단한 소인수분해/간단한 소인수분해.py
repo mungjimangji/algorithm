422bc54682fed85af8c241c69977ec42bbe5f1bc
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-03-20 23:15:00 +0900

    [D2] Title: 간단한 소인수분해, Time: 103 ms, Memory: 41,944 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,29 +1,19 @@
+prime_nums = [2, 3, 5, 7, 11]
+
 T = int(input())
 for t in range(1, T+1):
-
     N = int(input())
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    e = 0
-    while N % 2 == 0:
-        a += 1
-        N = N / 2
+    index = 0
+    divisor = [0] * 5
+    
+    for num in prime_nums:
+        prime_cnt = 0
 
-    while N % 3 == 0:
-        b += 1
-        N = N / 3
+        while N % num == 0:
+            prime_cnt += 1
+            N = N / num
 
-    while N % 5 == 0:
-        c += 1
-        N = N / 5
-
-    while N % 7 == 0:
-        d += 1
-        N = N / 7
-
-    while N % 11 == 0:
-        e += 1
-        N = N / 11
-    print(f"#{t} {a} {b} {c} {d} {e}")
+        divisor[index] = prime_cnt
+        index += 1
+    
+    print(f"#{t}", *divisor)
