72115f38d95e2b3d1d9abf2d07dfdc428caae702
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-03-24 03:47:53 +0900

    [D2] Title: 달팽이 숫자, Time: 126 ms, Memory: 46,376 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -0,0 +1,28 @@
+from collections import deque
+for tc in range(int(input())):
+    N = int(input())
+    result = [[0 for _ in range(N)] for _ in range(N)] #
+    deq = deque([(1,0), (0,1), (-1,0), (0,-1)]) #
+    cnt = 1
+    i = 0
+    j = 0
+
+    while cnt != N * N+1:
+        result[j][i] = cnt
+        x, y = deq[0]
+        i += x
+        j += y
+
+        if i < 0 or j < 0 or i > N-1 or j> N-1 or result[j][i] != 0: #못감
+            i -= x
+            j -= y
+            deq.append(deq.popleft())
+            x2, y2 = deq[0]
+            i += x2
+            j += y2
+
+        cnt += 1
+        
+    print(f'#{tc+1}')
+    for i in result:
+        print(*i)
