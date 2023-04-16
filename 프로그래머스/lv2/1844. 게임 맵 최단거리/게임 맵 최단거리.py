a729c7e213bfd50e4256681622fe062bcfc2364b
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-04-16 18:06:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,24 @@
 from collections import deque
-
 def solution(maps):
-    n, m = len(maps), len(maps[0])
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    return bfs(maps, 0, 0, visited)
-    
-def bfs(maps, x, y, visited):
-    n, m = len(maps), len(maps[0])
-    queue = deque([(x, y)])
-    visited[x][y] = True
-    distance = {(x, y): 0}
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and maps[nx][ny]:
-                if (nx, ny) == (n-1, m-1): return distance[(x, y)] + 2
-                queue.append((nx, ny))
-                distance[(nx, ny)] = distance[(x, y)] + 1
-                visited[nx][ny] = True
-    return -1
+    N = len(maps)
+    M = len(maps[0])
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    stack = deque([(0,0)])
+
+    while stack:
+        i, j = stack.popleft()
+
+        for n in range(4):
+            x = i + dx[n]
+            y = j + dy[n]
+            if 0 <= x < N and 0 <= y < M:
+                if maps[x][y] == 1:
+                    stack.append((x,y))
+                    maps[x][y] = maps[i][j] + 1
+    if maps[-1][-1] == 1:
+        answer = -1
+    else:
+        answer = maps[-1][-1]
+    return answer
