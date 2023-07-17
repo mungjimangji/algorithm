dc4c209cfe374e43a8caaf85a240bbc0f0f48d5e
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-07-17 13:53:54 +0900

    [Gold IV] Title: N-Queen, Time: 6952 ms, Memory: 125116 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,17 @@
+def dfs(n):
+    global result
+    if n == N:
+        result += 1
+        return
+    for j in range(N):
+        if v1[j] == v2[n+j] == v3[n-j] == 0:
+            v1[j] = v2[n+j] = v3[n-j] = 1
+            dfs(n+1)
+            v1[j] = v2[n+j] = v3[n-j] = 0
+N = int(input())
+result = 0
+v1 = [0] * N
+v2 = [0] * (2*N)
+v3 = [0] * (2*N)
+dfs(0)
+print(result)
