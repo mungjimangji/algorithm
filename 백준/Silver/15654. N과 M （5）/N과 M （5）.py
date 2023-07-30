32b5e5d36fae5af3e3f5e0b2a05e3961b865a8f6
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-07-30 22:47:36 +0900

    [Silver III] Title: N과 M (5), Time: 200 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+num_list = list(map(int, input().split()))
+num_list.sort()
+
+def backTracking(result):
+    if len(result) == M:
+        print(*result)
+        return
+    for i in num_list:
+        if i in result:
+            continue
+        result.append(i)
+        backTracking(result)
+        result.pop()
+backTracking([])
