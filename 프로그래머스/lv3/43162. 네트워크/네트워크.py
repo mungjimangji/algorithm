e8f4d4dae976b2b9b40745b6ea4ac7ad1c88513d
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-08-08 09:45:15 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,20 +1,18 @@
 def solution(n, computers):
     visited = [False] * n
     answer = 0
-    for i in range(n):
+    for i in range(n): # 0 1 2
         if visited[i] == False:
             stack = [i]
             visited[i] = True
-            while stack:
+            while stack: # 연결된 컴퓨터는 이 while문에서 모두 true
                 cur = stack.pop()
-                for j in range(n):
+                for j in range(n):# 0 1 2
                     if cur == j:
                         continue
                     elif computers[cur][j] == 1 and visited[j] == False:
                         visited[j] = True
-                        
                         stack.append(j)
             answer += 1        
-        
-
+    
     return answer
