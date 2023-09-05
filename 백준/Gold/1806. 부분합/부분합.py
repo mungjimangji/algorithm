da0c60a7fc4f48cccee59a537618c94c86165d33
mungjimangji <121420382+mungjimangji@users.noreply.github.com>
2023-09-05 09:33:45 +0900

    [Gold IV] Title: 부분합, Time: 104 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,31 +1,29 @@
 import sys
-# sys.stdin  = open('input.txt', 'r')
-from collections import deque
+input = sys.stdin.readline
 
+# 최소 길이 출력, 불가능 하면 0 출력
 N, S = map(int, input().split())
-numbers = list(map(int, input().split()))
+sequence = list(map(int, input().split()))
 
-visited = [False] * len(numbers)
+min_length = N+1
+start, end = 0, 0
+partial_sum = 0
 
-
-left, right = 0, 0 # 두 개의 포인터는 0에서 부터 시작
-total = 0 # 합을 저장할 변수
-min_length = 100000 # 먼저 최대 길이로 지정
 
 while True:
-    # 만약 총 합이 S가 넘는다면, left를 하나씩 옮겨보면서 어디까지 길이가 줄어드나 확인
-    if total >= S:
-        min_length = min(min_length, right - left)
-        total -= numbers[left]
-        left += 1
-    elif right == N:
+    # S보다 커지면
+    if partial_sum >= S:
+        if min_length > end - start:
+            min_length = end - start
+        partial_sum -= sequence[start]
+        start += 1
+    elif end == N:
         break
-    # 만약 총합이 S를 넘지 않는다면, right 을 오른쪽으로 한칸씩 옮기며 총합이 S를 넘을때까지 더함
-    else:
-        total += numbers[right]
-        right += 1
+    elif partial_sum < S:
+        partial_sum += sequence[end]
+        end += 1
 
-if min_length == 100000:
-    print(0)
-else:
+if min_length != N+1:
     print(min_length)
+else:
+    print(0)
