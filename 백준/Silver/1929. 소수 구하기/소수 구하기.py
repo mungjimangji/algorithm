c81f81ed76f851f4f534c740f8b0fd10e7d3e2c9
MinJi <121420382+mungjimangji@users.noreply.github.com>
2023-10-16 11:54:41 +0900

    [Silver III] Title: 소수 구하기, Time: 5252 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,13 @@
-m,n=map(int,input().split())
+N, M = map(int, input().split())
 
-for i in range(m,n+1):
-    if i==1:#1은 소수가 아니므로 제외
-        continue
-    for j in range(2,int(i**0.5)+1):
-        if i%j==0: #약수가 존재하므로 소수가 아님
-            break   #더이상 검사할 필요가 없으므로 멈춤
-    else:
+for i in range(N, M+1):
+    check = True
+    if i == 1:
+        continue   
+
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            check = False
+            break
+    if check:
         print(i)
-        
