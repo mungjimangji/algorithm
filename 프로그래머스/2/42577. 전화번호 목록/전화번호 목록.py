f1ea0a585866cae3f2444ef42c977e0c789262f1
MinJi <121420382+mungjimangji@users.noreply.github.com>
2023-11-23 02:32:45 +0900

    [level 2] Title: 전화번호 목록, Time: 86.30 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,8 @@
-def solution(phone_book):
-    answer = True
-    phone_book.sort()
-    # 정렬하면 다음 값이 더 길어야만 포함가능 함
-    for i in range(len(phone_book) - 1):
-        if phone_book[i] < phone_book[i+1]:
-            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
-                answer = False
-                return answer
-        
-        
-            
-    return answer
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+    
+    # ['119', '1195524421', '97674223'], ['1195524421', '97674223']
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
