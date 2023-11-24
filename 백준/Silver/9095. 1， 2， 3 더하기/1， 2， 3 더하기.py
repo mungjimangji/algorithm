7c9eca084ce9f2a3e129f2ec75ab4e9cd24324f7
MinJi <121420382+mungjimangji@users.noreply.github.com>
2023-11-25 02:28:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,14 +1,11 @@
-import sys
-read = sys.stdin.readline
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-cache = [0] * 11
-cache[1] = 1
-cache[2] = 2
-cache[3] = 4
 
 for i in range(4, 11):
-    cache[i] = sum(cache[i-3:i])
+    dp[i] = sum(dp[i-3:i])
 
-T = int(read())
-for _ in range(T):
-    print(cache[int(read())])
+for _ in range(int(input())):
+    print(dp[int(input())])
