9169d817402352a2dc11f652b68bbff1dc7b7bae
MinJi <121420382+mungjimangji@users.noreply.github.com>
2024-03-08 23:49:05 +0900

    [Bronze V] Title: 몇개고?, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.py
@@ -0,0 +1,7 @@
+T, S = map(int, input().split())
+
+if (12 <= T <= 16) and (S == 0) :
+    print(320)
+else :
+    print(280)
+# 출처: https://calkolab.tistory.com/entry/baekjoon-python-27294 [CALKO LAB:티스토리]
